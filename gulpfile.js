330dc156a2d96334631b4204dfbac5c83a8249b7
ved <prakashh108@gmail.com>
2019-05-30 19:09:33 +0530

    Second Commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,12 @@ const browsersync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const del = require("del");
 const gulpfile = require("gulp");
-const header = require("gulp-header");
 const merge = require("merge-stream");
 const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
 
-// Load package.json for banner
-const pkg = require('./package.json');
-
-// Set the banner content
-const banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-    ' */\n',
-    '\n'
-].join('');
 
 // BrowserSync
 function browserSync(done) {
@@ -51,19 +39,13 @@ function modules() {
     // Bootstrap
     var bootstrap = gulpfile.src('./node_modules/bootstrap/dist/**/*')
         .pipe(gulpfile.dest('./vendor/bootstrap'));
-    // Font Awesome
-    var fontAwesome = gulpfile.src('./node_modules/@fortawesome/**/*')
-        .pipe(gulpfile.dest('./vendor'));
-    // jQuery Easing
-    var jqueryEasing = gulpfile.src('./node_modules/jquery.easing/*.js')
-        .pipe(gulpfile.dest('./vendor/jquery-easing'));
     // jQuery
     var jquery = gulpfile.src([
         './node_modules/jquery/dist/*',
         '!./node_modules/jquery/dist/core.js'
     ])
         .pipe(gulpfile.dest('./vendor/jquery'));
-    return merge(bootstrap, fontAwesome, jquery, jqueryEasing);
+    return merge(bootstrap, jquery);
 }
 
 // CSS task
@@ -79,9 +61,6 @@ function css() {
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
-        }))
-        .pipe(header(banner, {
-            pkg: pkg
         }))
         .pipe(gulpfile.dest("./css"))
         .pipe(rename({
@@ -100,9 +79,7 @@ function js() {
             '!./js/*.min.js'
         ])
         .pipe(uglify())
-        .pipe(header(banner, {
-            pkg: pkg
-        }))
+
         .pipe(rename({
             suffix: '.min'
         }))
@@ -123,10 +100,4 @@ const build = gulpfile.series(vendor, gulpfile.parallel(css, js));
 const watch = gulpfile.series(build, gulpfile.parallel(watchFiles, browserSync));
 
 // Export tasks
-exports.css = css;
-exports.js = js;
-exports.clean = clean;
-exports.vendor = vendor;
-exports.build = build;
 exports.watch = watch;
-exports.default = build;
